00fa855df17fab3573f8a7c2cf9c1071bb7692c2
Teerayuth Artidakorn <bomzakup_123@hotmail.com>
2019-09-06 18:52:08 +0700

    path edit

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1200, 1000));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
